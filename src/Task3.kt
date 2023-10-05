4ea8d9e006f41d6ecb521e9bbb7765f3f4c8d31e
ReihanFatilla <reihanfatilla@gmail.com>
2023-10-05 12:49:16 +0700

    [Task-3]: finished task 3 with testing

--- src/Task3.kt
+++ src/Task3.kt
@@ -1,17 +1,25 @@
-import java.text.DateFormat
 import java.time.LocalDate
+import java.time.Period
 import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.test.assertEquals
 
-fun getDateDifference(before: String, after: String): String{
 
+fun getDateDifference(before: String, after: String): String{
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-d")
+    val dateBefore = LocalDate.parse(before, formatter)
+    val dateAfter = LocalDate.parse(after, formatter)
+
+    Period.between(dateBefore, dateAfter).also { period->
+        return "${period.years} Tahun, ${period.months} Bulan, ${period.days} Hari"
+    }
 }
 
 fun main(){
     val timeBefore = "2008-03-24"
     val timeAfter = "2010-06-26"
 
-    val expectedOutput = "2 Tahun, 3 Bulan, 4 Hari"
+    val expectedOutput = "2 Tahun, 3 Bulan, 2 Hari"
 
     val actualOutput = getDateDifference(timeBefore, timeAfter)
 
