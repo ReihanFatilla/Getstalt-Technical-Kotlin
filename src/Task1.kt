1ef9ed6ee743686e5f64b09a969364b56b4da210
ReihanFatilla <reihanfatilla@gmail.com>
2023-10-05 11:11:21 +0700

    [Task-1]: Finished Task 1

--- src/Task1.kt
+++ src/Task1.kt
@@ -1,21 +1,40 @@
 import kotlin.test.assertEquals
 
-fun generateLoopPattern(array: Array<String>): Array<Array<String>>{
+fun generateLoopPattern(array: ArrayList<String>): Array<Array<String>>{
+    return ArrayList<ArrayList<String>>().also { arrayList ->
+        arrayList.add(array)
+        repeat(array.size){ index ->
+            if(index == 0) return@repeat
+            val newArray = arrayList[index - 1].toTypedArray().clone()
+            if(index % 3 != 0){
+                newArray[0] = arrayList[index - 1][index]
+                newArray[index] = arrayList[index - 1][0]
+                arrayList.add(newArray.toCollection(ArrayList()))
+            } else {
+                val arr = newArray.clone().toMutableList()
+                arr.add(0, newArray[newArray.size - 1])
+                arr.removeLast()
+                arrayList.add(arr.toCollection(ArrayList()))
+            }
 
+        }
+    }.map { it.toTypedArray() }.toTypedArray()
 }
 
 fun main(){
-    val array = arrayOf("Volvo","BMW","Toyota","Kijang")
+    val array = arrayListOf("Volvo","BMW","Toyota","Kijang")
 
-    val expectedOutput = arrayOf(
-        arrayOf("Volvo","BMW","Toyota","Kijang"),
-        arrayOf("BMW","Volvo","Toyota","Kijang"),
-        arrayOf("Toyota","Volvo","BMW","Kijang"),
-        arrayOf("Kijang","Toyota","Volvo","BMW"),
+    val expectedOutput = listOf(
+        listOf("Volvo","BMW","Toyota","Kijang"),
+        listOf("BMW","Volvo","Toyota","Kijang"),
+        listOf("Toyota","Volvo","BMW","Kijang"),
+        listOf("Kijang","Toyota","Volvo","BMW"),
     )
 
-    val actualOutput = generateLoopPattern(array)
+    val actualOutput = generateLoopPattern(array).map { it.toList() }.toList()
 
-    print("result: $actualOutput")
-    assertEquals(expectedOutput, actualOutput)
+    print("result: ${actualOutput}")
+    assertEquals(expectedOutput, actualOutput, message = "Task Passed!")
 }
+
+
